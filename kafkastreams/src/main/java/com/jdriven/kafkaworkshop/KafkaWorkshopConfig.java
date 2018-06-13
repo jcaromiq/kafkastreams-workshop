3fa801ffb3f5f2b8a7c69e9ff681e5986bfe63bb
Joaquin Caro <jcaromiq@gmail.com>
2018-06-13 09:30:17 +0200

    Configure Kafka

--- kafkastreams/src/main/java/com/jdriven/kafkaworkshop/KafkaWorkshopConfig.java
+++ kafkastreams/src/main/java/com/jdriven/kafkaworkshop/KafkaWorkshopConfig.java
@@ -1,10 +1,42 @@
 package com.jdriven.kafkaworkshop;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaWorkshopConfig {
 
+    @Value("${kafka.bootstrap.servers}")
+    private String bootstrapServers;
+
+    @Bean
+    protected KafkaTemplate<String, SensorData> getKafkaTemplate() {
+        return new KafkaTemplate(getProducerFactory());
+    }
+
+    private ProducerFactory getProducerFactory() {
+        return new DefaultKafkaProducerFactory(getKafkaProducerConfig());
+    }
+
+    private Map<String, Object> getKafkaProducerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
 }
 
 
